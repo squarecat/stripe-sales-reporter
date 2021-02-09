20200113e837bb1a46a894ce4c2fc5e28dad16ad
Christopher Dengsø <work@cude.io>
2021-02-09 10:02:44 +0800

    Changed country codes to use ISO 3166

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var euList = [
   { country: "Finland", code: "FI", vat: 24 },
   { country: "France", code: "FR", vat: 20 },
   { country: "Germany", code: "DE", vat: 19 },
-  { country: "Greece", code: "EL", vat: 24 },
+  { country: "Greece", code: "GR", vat: 24 },
   { country: "Hungary", code: "HU", vat: 27 },
   { country: "Ireland", code: "IE", vat: 23 },
   { country: "Italy", code: "IT", vat: 22 },
@@ -24,13 +24,13 @@ var euList = [
   { country: "Luxembourg", code: "LU", vat: 17 },
   { country: "Malta", code: "MT", vat: 18 },
   { country: "Netherlands", code: "NL", vat: 21 },
-  { country: "Poland", code: "PO", vat: 23 },
+  { country: "Poland", code: "PL", vat: 23 },
   { country: "Portugal", code: "PT", vat: 23 },
   { country: "Romania", code: "RO", vat: 20 },
   { country: "Slovakia", code: "SK", vat: 20 },
   { country: "Slovenia", code: "SI", vat: 22 },
   { country: "Spain", code: "ES", vat: 21 },
-  { country: "Sweden", code: "SW", vat: 25 },
+  { country: "Sweden", code: "SE", vat: 25 },
   { country: "United Kingdom", code: "GB", vat: 20 }
 ];
 
